db4fc4fd7caeabad50bc49a349d23cf653b9aeb8
nasrathf2001 <nasrath.f@iqhired.com>
2023-08-31 16:59:03 +0530

    graph sample design with dummy data

--- device/view_device_dashboard.php
+++ device/view_device_dashboard.php
@@ -86,6 +86,9 @@
     <link rel="stylesheet" href="<?php echo $iotURL; ?>assets/pages/css/vDeviceDashboard.css">
 <!--    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.1.1/Chart.min.js"></script>-->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/1.0.2/Chart.min.js"></script>
+    <script src="https://cdn.canvasjs.com/canvasjs.min.js"></script>
+
+
 
 </head>
 <?php include('./../header.php'); ?>
@@ -221,6 +224,9 @@
                             </div>
                         </div>
 					<?php } ?>
+
+                <div id="chartContainer" style="height: 370px; width: 100%;"></div>
+
             </div>
 
         </div>
@@ -229,6 +235,39 @@
 
 </div>
 </a>
+<script>
+    window.onload = function () {
+
+        var chart = new CanvasJS.Chart("chartContainer", {
+            animationEnabled: true,
+            theme: "light2",
+            title:{
+                text: "Trend Graph"
+            },
+            data: [{
+                type: "line",
+                indexLabelFontSize: 16,
+                dataPoints: [
+                    { y: 450 },
+                    { y: 414},
+                    { y: 520, indexLabel: "\u2191 highest",markerColor: "red", markerType: "triangle" },
+                    { y: 460 },
+                    { y: 450 },
+                    { y: 500 },
+                    { y: 480 },
+                    { y: 480 },
+                    { y: 410 , indexLabel: "\u2193 lowest",markerColor: "DarkSlateGrey", markerType: "cross" },
+                    { y: 500 },
+                    { y: 480 },
+                    { y: 510 }
+                ]
+            }]
+        });
+
+        chart.render();
+
+    }
+</script>
 <!----------------->
 <script>
     Chart.types.Doughnut.extend({
